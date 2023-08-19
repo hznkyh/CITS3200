dbb2b4147a7992031d2e5c20d65735d551f7dd01
hznkyh <hank021008@outlook.com>
2023-08-19 14:33:32 +0800

    changed to template literals

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -1,5 +1,10 @@
 Vue.component('header-component', {
-  template: '<div class="navigation"><a id="nav_home" class="active" href="main.html">Home</a><a id="nav_simulator" class="inactive" href="simulator.html">Simulator</a></div>'
+  template: `
+            <div class="navigation">
+              <a id="nav_home" class="active" href="main.html">Home</a>
+              <a id="nav_simulator" class="inactive" href="simulator.html">Simulator</a>
+            </div>
+            `
 });
 
 var vm = new Vue({
