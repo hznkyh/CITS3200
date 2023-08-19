beef6509546bf89fdbae03320f11ce1b66cccdf9
hznkyh <hank021008@outlook.com>
2023-08-19 14:19:18 +0800

    extend header with vue component

---
+++ frontend/static/main.js
@@ -0,0 +1,7 @@
+Vue.component('header-component', {
+  template: '<div class="navigation"><a id="nav_home" class="active" href="main.html">Home</a><a id="nav_simulator" class="inactive" href="simulator.html">Simulator</a></div>'
+});
+
+var vm = new Vue({
+  el: '#app',
+})
