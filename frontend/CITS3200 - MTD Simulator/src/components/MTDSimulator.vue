81d6e4e7bf5873837352933c158fb0dee09899e9
Jeremy Robertson <shadow102024@bigpond.com>
2023-08-22 10:58:16 +0800

    Start of page layout

--- frontend/CITS3200 - MTD Simulator/src/components/MTDSimulator.vue
+++ frontend/CITS3200 - MTD Simulator/src/components/MTDSimulator.vue
@@ -1,5 +1,21 @@
 <template>
-    <h1>MTD Simulator</h1>
+    <header>
+  <h2>MTD Simultor</h2>
+</header>
+
+<section>
+  <form>
+    <h2> Parameters Panel</h2>
+  </form>
+  
+  <article>
+    <h1>London</h1>
+  </article>
+</section>
+
+<footer>
+  <p>Footer</p>
+</footer>
 </template>
 
 <script>
@@ -9,4 +25,49 @@ export default {
 </script>
 
 <style>
+  * {
+  box-sizing: border-box;
+}
+
+/* Style the header */
+header {
+  background-color: rgb(12, 69, 225);
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+
+form {
+  float: left;
+  width: 30%;
+  background: #ccc;
+  padding: 20px;
+}
+
+article {
+  float: left;
+  padding: 20px;
+  width: 70%;
+  background-color: #f1f1f1;
+  height: auto;
+}
+
+footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+@media (max-width: 600px) {
+  form, article {
+    width: 100%;
+    height: auto;
+  }
+}
+
 </style>
