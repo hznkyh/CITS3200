41ea7066c2f3c3358928a61334cdc8e13402b394
Jeremy Robertson <shadow102024@bigpond.com>
2023-08-29 15:06:18 +0800

    Made basic changes to Home page in similar style to MTD Sim page

--- frontend/CITS3200 - MTD Simulator/src/components/Home.vue
+++ frontend/CITS3200 - MTD Simulator/src/components/Home.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
-        <h1>Home</h1>
+      <header>
+        <h2>Home</h2>
+      </header>
     </div>
+    <p>This is a test sentence.</p>
+    <p>This is a test sentence.</p>
+    <p>This is a test sentence.</p>
+    <p>This is a test sentence.</p>
+    <p>This is a test sentence.</p>
+    <p>This is a test sentence.</p>
+    <footer></footer>
 </template>
 
 <script>
@@ -11,4 +20,19 @@ export default {
 </script>
 
 <style>
+header {
+  background-color: rgb(12, 69, 225);
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+footer {
+  flex-shrink: 0;
+  background-color: #777;
+  text-align: center;
+  color: white;
+  bottom: 0;
+  padding: 10px;
+}
 </style>
