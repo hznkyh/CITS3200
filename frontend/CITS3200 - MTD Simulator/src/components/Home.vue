95a6c09618f00bd267f760fe217be2fdb84b11a7
hznkyh <hank021008@outlook.com>
2023-09-02 15:23:02 +0800

    moved style to app.vue

--- frontend/CITS3200 - MTD Simulator/src/components/Home.vue
+++ frontend/CITS3200 - MTD Simulator/src/components/Home.vue
@@ -32,19 +32,4 @@ export default {
 </script>
 
 <style>
-header {
-  background-color: rgb(12, 69, 225);
-  padding: 30px;
-  text-align: center;
-  font-size: 35px;
-  color: white;
-}
-footer {
-  flex-shrink: 0;
-  background-color: #777;
-  text-align: center;
-  color: white;
-  bottom: 0;
-  padding: 10px;
-}
 </style>
