5dfcd4473110d5cbd30614816c21e410a7e3dd7c
hznkyh <hank021008@outlook.com>
2023-09-02 16:54:35 +0800

    Integration of backend and frontend

--- frontend/CITS3200 - MTD Simulator/src/main.js
+++ frontend/CITS3200 - MTD Simulator/src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import VNetworkGraph from "v-network-graph"
 import "v-network-graph/lib/style.css"
 import App from './App.vue'
+
 import Home from './components/Home.vue'
 import MTDSimulator from './components/MTDSimulator.vue'
 
+import axios from 'axios'
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8001/'
 
 const app = createApp(App)
 app.component('home', Home)
