3c971321c0dcc31a50ea0de95728bf5b5304464f
hznkyh <hank021008@outlook.com>
2023-09-02 15:11:23 +0800

    add left to right graph

--- frontend/CITS3200 - MTD Simulator/src/components/Network.vue
+++ frontend/CITS3200 - MTD Simulator/src/components/Network.vue
@@ -1,30 +1,97 @@
 <script setup lang="ts">
-  const nodes = {
-    node1: { name: "Node 1" },
-    node2: { name: "Node 2" },
-    node3: { name: "Node 3" },
-    node4: { name: "Node 4" },
+import { ref } from "vue"
+import * as vNG from "v-network-graph"
+import data from "./data"
+
+// dagre: Directed graph layout for JavaScript
+// https://github.com/dagrejs/dagre
+//@ts-ignore
+import dagre from "dagre/dist/dagre.min.js"
+
+const nodeSize = 40
+
+const configs = vNG.defineConfigs({
+  view: {
+    autoPanAndZoomOnLoad: "fit-content",
+    onBeforeInitialDisplay: () => layout("LR"),
+  },
+  node: {
+    normal: { radius: nodeSize / 2 },
+    label: { direction: "center", color: "#fff" },
+  },
+  edge: {
+    normal: {
+      color: "#aaa",
+      width: 3,
+    },
+    margin: 4,
+    marker: {
+      target: {
+        type: "arrow",
+        width: 4,
+        height: 4,
+      },
+    },
+  },
+})
+
+const graph = ref<vNG.VNetworkGraphInstance>()
+
+function layout(direction: "TB" | "LR") {
+  if (Object.keys(data.nodes).length <= 1 || Object.keys(data.edges).length == 0) {
+    return
   }
 
-  const edges = {
-    edge1: { source: "node1", target: "node2" },
-    edge2: { source: "node2", target: "node3" },
-    edge3: { source: "node3", target: "node4" },
-  }
+  // convert graph
+  // ref: https://github.com/dagrejs/dagre/wiki
+  const g = new dagre.graphlib.Graph()
+  // Set an object for the graph label
+  g.setGraph({
+    rankdir: direction,
+    nodesep: nodeSize * 2,
+    edgesep: nodeSize,
+    ranksep: nodeSize * 2,
+  })
+  // Default to assigning a new object as a label for each new edge.
+  g.setDefaultEdgeLabel(() => ({}))
+
+  // Add nodes to the graph. The first argument is the node id. The second is
+  // metadata about the node. In this case we're going to add labels to each of
+  // our nodes.
+  Object.entries(data.nodes).forEach(([nodeId, node]) => {
+    g.setNode(nodeId, { label: node.name, width: nodeSize, height: nodeSize })
+  })
+
+  // Add edges to the graph.
+  Object.values(data.edges).forEach(edge => {
+    g.setEdge(edge.source, edge.target)
+  })
+
+  dagre.layout(g)
+
+  g.nodes().forEach((nodeId: string) => {
+    // update node position
+    const x = g.node(nodeId).x
+    const y = g.node(nodeId).y
+    data.layouts.nodes[nodeId] = { x, y }
+  })
+}
+
+function updateLayout(direction: "TB" | "LR") {
+  // Animates the movement of an element.
+  graph.value?.transitionWhile(() => {
+    layout(direction)
+  })
+}
 </script>
 
 <template>
   <v-network-graph
+    ref="graph"
     class="graph"
-    :nodes="nodes"
-    :edges="edges"
+    :nodes="data.nodes"
+    :edges="data.edges"
+    :layouts="data.layouts"
+    :configs="configs"
   />
 </template>
-
-<style>
-.graph {
-  width: 800px;
-  height: 600px;
-  border: 1px solid #000;
-}
-</style>
