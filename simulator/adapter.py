2b5ded2978d30663ebb612794ea868f2115fb87e
NoTouchingKids <kp25179@gmail.com>
2023-09-04 17:59:46 +0800

    style(sim): Reformat code and fix indentation
    - Applied PEP 8 styling to function in the `adapter.py`.
    - Fixed inconsistent indentation in `create_sim` function.
    
    No changes to logic or functionality.

--- simulator/adapter.py
+++ simulator/adapter.py
@@ -171,120 +171,85 @@ def execute_sim(start_time=0, finish_time=None, scheme='random', mtd_interval=No
     return res
 
 
-def run_sim(env: simpy.Environment, start_time=0, finish_time=None, scheme='random', mtd_interval=None, custom_strategies=None,
-            checkpoints=None, total_nodes=50, total_endpoints=5, total_subnets=8, total_layers=4,
-            target_layer=4, total_database=2, terminate_compromise_ratio=0.8, new_network=False):
-    """
-
-    :param start_time: the time to start the simulation, need to load timestamp-based snapshots if set start_time > 0
-    :param finish_time: the time to finish the simulation. Set to None will run the simulation until
-    the network reached compromised threshold (compromise ratio > 0.9)
-    :param scheme: random, simultaneous, alternative, single, None
-    :param mtd_interval: the time interval to trigger an MTD(s)
-    :param custom_strategies: used for executing alternative scheme or single mtd strategy.
-    :param checkpoints: a list of time value to save snapshots as the simulation runs.
-    :param total_nodes: the number of nodes in the network (network size)
-    :param total_endpoints: the number of exposed nodes
-    :param total_subnets: the number of subnets (total_nodes - total_endpoints) / (total_subnets - 1) > 2
-    :param total_layers: the number of layers in the network
-    :param target_layer: the target layer in the network (for targetted attack scenario only)
-    :param total_database: the number of database nodes used for computing DAP algorithm
-    :param terminate_compromise_ratio: terminate the simulation if reached compromise ratio
-    :param new_network: True: create new snapshots based on network size, False: load snapshots based on network size
-    """
-    # initialise the simulation
-    # env = simpy.Environment()
-    end_event = env.event()
-    print("env is ")
-    print(env)
-    snapshot_checkpoint = SnapshotCheckpoint(env=env, checkpoints=checkpoints)
-    time_network = None
-    adversary = None
-    res = list()
-    if start_time > 0:
-        try:
-            time_network, adversary = snapshot_checkpoint.load_snapshots_by_time(
-                start_time)
-        except FileNotFoundError:
-            print('No timestamp-based snapshots available! Set start_time = 0 !')
-            return
-    elif not new_network:
-        try:
-            time_network, adversary = snapshot_checkpoint.load_snapshots_by_network_size(
-                total_nodes)
-        except FileNotFoundError:
-            print('set new_network=True')
-    else:
-        time_network = TimeNetwork(total_nodes=total_nodes, total_endpoints=total_endpoints,
-                                   total_subnets=total_subnets, total_layers=total_layers,
-                                   target_layer=target_layer, total_database=total_database,
-                                   terminate_compromise_ratio=terminate_compromise_ratio)
-        adversary = Adversary(network=time_network,
-                              attack_threshold=ATTACKER_THRESHOLD)
-        # snapshot_checkpoint.save_initialised(time_network, adversary)
-        snapshot_checkpoint.save_snapshots_by_network_size(
-            time_network, adversary)
-
-    # start attack
-    attack_operation = AttackOperation(
-        env=env, end_event=end_event, adversary=adversary, proceed_time=0)
-    print(env)
-    attack_operation.proceed_attack()
-
-    # start mtd
-    if scheme != 'None':
-        mtd_operation = MTDOperation(env=env, end_event=end_event, network=time_network, scheme=scheme,
-                                     attack_operation=attack_operation, proceed_time=0,
-                                     mtd_trigger_interval=mtd_interval, custom_strategies=custom_strategies)
-        mtd_operation.proceed_mtd()
-
-    # save snapshot by time
-    if checkpoints is not None:
-        snapshot_checkpoint.proceed_save(time_network, adversary)
-    env.process(get_results(time_network=time_network, env=env,
-                start_time=start_time, finish_time=finish_time, res=res))
-    # start simulation
-    if finish_time is not None:
-        env.run(until=(finish_time - start_time))
-        print("ENDED1")
-    else:
-        env.run(until=end_event)
-        print("ENDED2")
-    evaluation = Evaluation(network=time_network, adversary=adversary)
-
-    # sim_item = scheme
-    # if scheme == 'single':
-    #     sim_item = custom_strategies().get_name()
-    # elif scheme == 'None':
-    #     sim_item = 'NoMTD'
-    # time_network.get_mtd_stats().save_record(sim_time=mtd_interval, scheme=sim_item)
-    # adversary.get_attack_stats().save_record(sim_time=mtd_interval, scheme=sim_item)
-    return evaluation , res
-
-
-def create_sim(env: simpy.Environment, start_time=0, finish_time=None, scheme='random', mtd_interval=None, custom_strategies=None,
-            checkpoints=None, total_nodes=50, total_endpoints=5, total_subnets=8, total_layers=4,
-            target_layer=4, total_database=2, terminate_compromise_ratio=0.8, new_network=False):
-    """
-
-    :param start_time: the time to start the simulation, need to load timestamp-based snapshots if set start_time > 0
-    :param finish_time: the time to finish the simulation. Set to None will run the simulation until
-    the network reached compromised threshold (compromise ratio > 0.9)
-    :param scheme: random, simultaneous, alternative, single, None
-    :param mtd_interval: the time interval to trigger an MTD(s)
-    :param custom_strategies: used for executing alternative scheme or single mtd strategy.
-    :param checkpoints: a list of time value to save snapshots as the simulation runs.
-    :param total_nodes: the number of nodes in the network (network size)
-    :param total_endpoints: the number of exposed nodes
-    :param total_subnets: the number of subnets (total_nodes - total_endpoints) / (total_subnets - 1) > 2
-    :param total_layers: the number of layers in the network
-    :param target_layer: the target layer in the network (for targetted attack scenario only)
-    :param total_database: the number of database nodes used for computing DAP algorithm
-    :param terminate_compromise_ratio: terminate the simulation if reached compromise ratio
-    :param new_network: True: create new snapshots based on network size, False: load snapshots based on network size
-    """
-    # initialise the simulation
-    # env = simpy.Environment()
+def create_sim(
+    env: simpy.Environment,
+    start_time=0,
+    finish_time=None,
+    scheme='random',
+    mtd_interval=None,
+    custom_strategies=None,
+    checkpoints=None,
+    total_nodes=50,
+    total_endpoints=5,
+    total_subnets=8,
+    total_layers=4,
+    target_layer=4,
+    total_database=2,
+    terminate_compromise_ratio=0.8,
+    new_network=False
+) -> tuple(Evaluation, list):
+    '''The `create_sim` function creates a simulation environment for a network attack and defense
+    scenario, with options for different attack and defense strategies, network parameters, and
+    checkpointing.
+    
+    Parameters
+    ----------
+    env : simpy.Environment
+        The simpy environment in which the simulation will run.
+    start_time, optional
+        The start time at which the simulation should begin. If set to a value greater than 0, the
+    simulation will load snapshots of the network and adversary at that time. If set to 0, the
+    simulation will start with a new network.
+    finish_time
+        The time at which the simulation will finish. If not specified, the simulation will run
+    indefinitely.
+    scheme, optional
+        The `scheme` parameter determines the MTD (Moving Target Defense) scheme to be used. It can take
+    the following values:
+    mtd_interval
+        The `mtd_interval` parameter is the interval at which the MTD (Moving Target Defense) operation is
+    triggered. It determines how often the MTD strategies are applied to the network to mitigate
+    attacks.
+    custom_strategies
+        The `custom_strategies` parameter is a list that allows you to specify custom MTD strategies to be
+    used in the simulation. These strategies can be defined as functions and will be executed during the
+    MTD operation. Each strategy function should take the following parameters:
+    checkpoints
+        A list of time points at which to save snapshots of the network and adversary state.
+    total_nodes, optional
+        The total number of nodes in the network.
+    total_endpoints, optional
+        The parameter "total_endpoints" represents the total number of endpoints in the simulated network.
+    Endpoints are devices or systems that are connected to the network and can be potential targets for
+    attacks.
+    total_subnets, optional
+        The parameter "total_subnets" represents the total number of subnets in the network. A subnet is a
+    portion of a network that shares a common network address. It is used to divide a large network into
+    smaller, more manageable parts.
+    total_layers, optional
+        The parameter "total_layers" represents the total number of layers in the network. Each layer
+    represents a level of hierarchy in the network topology. For example, in a hierarchical network
+    architecture, there may be multiple layers of switches or routers, with each layer providing
+    connectivity to a subset of nodes in the network
+    target_layer, optional
+        The parameter "target_layer" specifies the layer in the network where the attack is targeted.
+    total_database, optional
+        The parameter "total_database" represents the total number of databases in the simulation.
+    terminate_compromise_ratio
+        The `terminate_compromise_ratio` parameter is used to determine the threshold at which the attack
+    is considered successful. It represents the ratio of compromised nodes in the network that will
+    trigger the termination of the attack. For example, if `terminate_compromise_ratio` is set to 0.8,
+    the
+    new_network, optional
+        A boolean value indicating whether to create a new network or load an existing one. If set to True,
+    a new network will be created. If set to False, an existing network will be loaded if available,
+    otherwise an error message will be printed.
+    
+    Returns
+    -------
+        The function `create_sim` returns two values: `evaluation` and `res`.
+    
+    '''
     end_event = env.event()
     snapshot_checkpoint = SnapshotCheckpoint(env=env, checkpoints=checkpoints)
     time_network = None
@@ -310,9 +275,8 @@ def create_sim(env: simpy.Environment, start_time=0, finish_time=None, scheme='r
                                    terminate_compromise_ratio=terminate_compromise_ratio)
         adversary = Adversary(network=time_network,
                               attack_threshold=ATTACKER_THRESHOLD)
-        snapshot_checkpoint.save_to_array(time_network, adversary,res)
-        # snapshot_checkpoint.save_snapshots_by_network_size(
-        #     time_network, adversary)
+        snapshot_checkpoint.save_to_array(time_network, adversary, res)
+
 
     # start attack
     attack_operation = AttackOperation(
@@ -329,17 +293,8 @@ def create_sim(env: simpy.Environment, start_time=0, finish_time=None, scheme='r
     # save snapshot by time
     if checkpoints is not None:
         snapshot_checkpoint.proceed_save(time_network, adversary)
-    # env.process(get_results(time_network=time_network, env=env,
-    #             start_time=start_time, finish_time=finish_time, res=res))
 
-    # # start simulation
-    # if finish_time is not None:
-    #     env.run(until=(finish_time - start_time))
-    #     print("ENDED1")
-    # else:
-    #     env.run(until=end_event)
-    #     print("ENDED2")
-    
+    # Evaluate the simulation
     evaluation = Evaluation(network=time_network, adversary=adversary)
-    
-    return evaluation , res
+
+    return evaluation, res
