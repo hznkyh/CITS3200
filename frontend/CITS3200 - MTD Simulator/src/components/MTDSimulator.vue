17ad0a0b4f4024420f40af6df29cbbcd762c420d
Jeremy Robertson <shadow102024@bigpond.com>
2023-09-23 17:51:52 +0800

    Changed design to white template to work with and changed tooltips

--- frontend/CITS3200 - MTD Simulator/src/components/MTDSimulator.vue
+++ frontend/CITS3200 - MTD Simulator/src/components/MTDSimulator.vue
@@ -5,56 +5,66 @@
         <h2> Parameters Panel</h2>
         <div>
           <label>Number of Nodes *:</label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">The number of nodes in the network (network size).</span>
             </span>
-            <span class="tip">The number of nodes in the network (network size).</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="Number of Nodes..." v-model="nodeNumber" name="total_nodes" required>
         </div>
         
         <div>
           <label>Number of Exposed Nodes *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">The number of exposed nodes in the network.</span>
             </span>
-            <span class="tip">The number of exposed nodes in the network.</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="Number of Exposed Nodes..." v-model="nodeExposed" name="total_endpoints" required>
         </div>
 
         <div>
           <label>Number of Layers *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">The number of layers in the network</span>
             </span>
-            <span class="tip">The number of layers in the network</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="Number of Layers..." v-model="layers" name="total_layers" required>
         </div>
 
         <div>
           <label>Compromise Ratio *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">The ratio number that will terminate the simulation if reached.</span>
             </span>
-            <span class="tip">The ratio number that will terminate the simulation if reached.</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="Compromise Ratio..." v-model="compromisedRatio" name="terminate_compromise_ratio" required>
         </div>
 
         <div>
           <label>Scheme *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">How the simulator will run. Chose from: random (default), simultaneous, alternative, single and none.</span>
             </span>
-            <span class="tip">How the simulator will run. Chose from: random (default), simultaneous, alternative, single and none.</span>
-          </span>
+          </div>
           <select id="param" type="text" placeholder="Scheme..." v-model="scheme" name="scheme" required>
             <option value="random">random</option>
             <option value="simultaneous"> simultaneous</option>
@@ -66,34 +76,40 @@
 
         <div>
           <label>MTD Interval *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">The time interval to trigger an MTD(s)</span>
             </span>
-            <span class="tip">The time interval to trigger an MTD(s)</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="MTD Interval..." v-model="interval" name="mtd_interval" required>
         </div>
 
         <div>
           <label>Finish Time *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">How long the simulation should run for</span>
             </span>
-            <span class="tip">How long the simulation should run for</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="Finish Time..." v-model="finishTime" name="finish_time" required>
         </div>
 
         <div>
           <label>Checkpoints *: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+              </span>
+              <span class="tip">How often snapshots are taken of the simulation</span>
             </span>
-            <span class="tip">How often snapshots are taken of the simulation</span>
-          </span>
+          </div>
           <input id="param" type="text" placeholder="Checkpoints..." v-model="checkpoints" name="checkpoints" required>
         </div>
 
@@ -101,35 +117,41 @@
         <div id="advancedPanel" class="hidden">
           <div>
           <label>Total Subnets: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
-            </span>
-            <span class="tip">The number of subnets</span>
-          </span>
-          <input id="param" type="text" placeholder="Total Subnets..." v-model="totalSubnets" name="total_subnets">
-        </div>
-
-        <div>
-          <label>Target Layers: </label>
-          <span id="tooltip">
-            <span class='info'>
-              <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
-            </span>
-            <span class="tip">How often snapshots are taken of the simulation</span>
-          </span>
-          <input id="param" type="text" placeholder="Target Layers..." v-model="targetLayers" name="target_layers">
-        </div>
-
-          <div>
+          <div class="tooltip-container">
             <span id="tooltip">
-              <label id="heading">MTD Priority</label>
               <span class='info'>
                 <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
               </span>
-              <span class="tip">Below you have the choice to change the priority of options the defender has to avoid an attack. By default (leave empty) it is ordered 1-7 from top to bottom. If you wish
-                to change the order please fill in the form.
+              <span class="tip">The number of subnets</span>
+            </span>
+          </div>
+          <input id="param" type="text" placeholder="Total Subnets..." v-model="totalSubnets" name="total_subnets">
+        </div>
+
+        <div >
+          <label>Target Layers: </label>
+          <div class="tooltip-container">
+            <span id="tooltip">
+              <span class='info'>
+                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
               </span>
+              <span class="tip">How often snapshots are taken of the simulation</span>
+            </span>
+          </div>
+          <input id="param" type="text" placeholder="Target Layers..." v-model="targetLayers" name="target_layers">
+        </div>
+
+          <div >
+            <span id="tooltip">
+              <label id="heading">MTD Priority</label>
+              <div class="tooltip-container">
+                <span class='info'>
+                  <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+                </span>
+                <span class="tip">Below you have the choice to change the priority of options the defender has to avoid an attack. By default (leave empty) it is ordered 1-7 from top to bottom. If you wish
+                  to change the order please fill in the form.
+                </span>
+            </div>
             </span>
           </div>
 
@@ -157,11 +179,13 @@
           <div>
             <span id="tooltip">
               <label id="heading">MTD Trigger Interval</label>
-              <span class='info'>
-                <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
-              </span>
-              <span class="tip">When to trigger the chosen scheme
-              </span>
+              <div class="tooltip-container">
+                <span class='info'>
+                  <img src='https://s3.lightboxcdn.com/vendors/906a5d64-2cda-407f-a2d5-6cf94c06ddbe/uploads/274a7932-a0fd-4a89-9f58-a83cc44112ca/info.svg' width='15' height='15'>
+                </span>
+                <span class="tip">When to trigger the chosen scheme
+                </span>
+              </div>
             </span>
           </div>
 
@@ -493,15 +517,21 @@ export default {
 </script>
 
 <style>
-  * {
+* {
   box-sizing: border-box; 
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f7f7f7;
 }
 
 #app{
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  overflow: hidden;
 }
 
 .content{
@@ -514,26 +544,38 @@ export default {
 .panel{
   float: left;
   width: 30%;
-  background: #ccc;
+  background: #ffffff;
   padding: 2em;
-  max-height: 100%;
-  border-radius: 35px;
+  border-radius: 10px;
   margin:2em;
   overflow-y: scroll;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .panel::-webkit-scrollbar {
-  border: 0;
+  width:8px;
 }
 
-.advanced {
-  border: 0px;
+.panel::-webkit-scrollbar-thumb {
   background-color: #ccc;
+  border-radius: 4px;
+}
+
+.panel::-webkit-scrollbar-thumb:hover{
+  background-color: #aaa;
+}
+
+
+.advanced {
+  background-color: #ffffff;
   display: inline-block;
-  padding: 0;
-  margin: 0 0 0.5em 0;
+  padding: 0.5em 1em;
+  margin:0 0 1em 0;
   width: 100%;
   text-align: left;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
 }
 
 .hidden{
@@ -544,35 +586,77 @@ export default {
   float: left;
   padding: 20px;
   width: 70%;
-  height: auto;
   text-align: center;
-  /* border: 2px solid #ccc; */
-  padding:0;
-  margin:1em;
+  margin: 2em;
   margin-bottom: 6em;
-  /* margin-bottom: 4em; */
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 input[type=text] {
    width: 100%;
    margin: 5px 0 10px 0;
+   padding: 10px;
    display: inline-block;
    border: none;
-   background: #f1f1f1;
+   background: #f5f5ff;
+   border-radius: 5px;
+   font-size: 16px;
 }
 
 input[type=text]:focus {
-   background-color: #ddd;
+   background-color: #e0e0e0;
    outline: none;
 }
-
-@media (max-width: 600px) {
-  form, article {
-    width: 100%;
-    height: auto;
-  }
+input[type="submit"]{
+  background-color: #000;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
 }
 
+input[type="submit"]:hover {
+  background-color: #333;
+}
+
+form {
+  padding: 1em;
+}
+
+.tooltip-container{
+  position: relative;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.tooltip-container .info{
+  cursor: pointer;
+  padding: 0 5px;
+  display: inline-block;
+}
+
+.tooltip-container .tip {
+  position: absolute;
+  top: 0;
+  left: 100%;
+  display: none;
+  min-width: 200px;
+  padding: 8px;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.tooltip-container:hover .tip{
+  display: block;
+}
 #tooltip{
     text-align: left;
     background-color: transparent;
@@ -584,33 +668,34 @@ input[type=text]:focus {
 
 #tooltip .info {
   text-decoration: underline;
-  font-style: Italic;
+  font-style: italic;
 }
 
 #tooltip .tip { 
-     display: none; 
+     display: none;
 }
 
  #tooltip:hover .tip { 
     display: block;
-    color: #514f4e;
-    background: white;
-    padding: 3px;
-    border: 1px solid #514f4e;
-    width: auto
+    color: #333;
+    background: #fff;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
  }
 
  #heading{
   font-size: 20px;
-  color: black;
+  color: #333;
  }
 
  .instructions {
     margin: 2em;
     margin-top: 0;
     padding: 2em;
-    border: 2px solid #ccc;
-    border-radius: 35px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
  }
 
 .list {
@@ -624,4 +709,5 @@ input[type=text]:focus {
   border-radius: 50%;
   display: inline-block;
 }
+
 </style>
