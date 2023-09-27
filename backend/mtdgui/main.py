a3a86d6cfab897796f02742bb49963fd5f50b198
hznkyh <hank021008@outlook.com>
2023-09-27 16:34:53 +0800

    fixed posting issue from frontend

--- backend/mtdgui/main.py
+++ backend/mtdgui/main.py
@@ -31,7 +31,7 @@ app.include_router(webSocketDev.router)
 
 app.add_middleware(
     CORSMiddleware,    
-    allow_origins= "http://localhost:5173",    
+    allow_origins= "http://localhost:8080/",    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],    
