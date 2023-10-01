94de3f2d0b72c3cc75b7ca4a0611a05e13379293
DanCUWA <chengdct@gmail.com>
2023-10-01 16:06:42 +0800

    Changes to format

--- frontend/Vite/src/components/MTDSimulator.vue
+++ frontend/Vite/src/components/MTDSimulator.vue
@@ -406,8 +406,8 @@ export default {
           "config": this.checkAdvancedDataEntered()
         };
         this.msg = 'Getting graph';
-        var data = JSON.stringify(mainData);
-        this.savedForms.push(data);
+        // var data = JSON.stringify(mainData);
+        this.savedForms.push(mainData);
         console.log(this.savedForms);
       }
 
@@ -428,8 +428,9 @@ export default {
       this.msg = 'Getting graph(s)';
 
       for (const savedForm of this.savedForms) {
-        const formData = JSON.parse(savedForm);
-
+        // const formData = JSON.parse(savedForm);
+        var formData = savedForm;
+        console.log("form data is:: " + formData);
         axios
           .post('/network/multi-graph', formData, {
             headers: { 'Content-Type': 'application/json' },
