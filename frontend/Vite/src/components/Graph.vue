189177b5ef25745bbdc58f4278a56f95e5e3c2b4
Jeremy Robertson <shadow102024@bigpond.com>
2023-10-05 14:54:12 +0800

    Made buttons more visible

--- frontend/Vite/src/components/Graph.vue
+++ frontend/Vite/src/components/Graph.vue
@@ -888,6 +888,11 @@
         border-radius: 5px;
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         cursor: pointer;
+        box-shadow: 
+            inset -2px -2px 5px rgba(255, 255, 255, 0.9), 
+            inset 2px 2px 5px rgba(0, 0, 0, 0.2),
+            -2px -2px 5px rgba(255, 255, 255, 0.9), 
+            2px 2px 5px rgba(0, 0, 0, 0.2);
     }
 
     .row{
