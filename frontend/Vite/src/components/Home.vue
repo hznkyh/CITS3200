ca47d6012499f8c0566b7c7ceb044f506e22840f
Jeremy Robertson <shadow102024@bigpond.com>
2023-10-05 14:54:30 +0800

    Added clients project specification

--- frontend/Vite/src/components/Home.vue
+++ frontend/Vite/src/components/Home.vue
@@ -2,6 +2,10 @@
   <div class="container">
     <p class="heading">MOVING TARGET DEFENCE SIMULATOR</p>
     <p>Welcome to the Moving Target Defence Simulator page!</p>
+    <p>Moving target defence is a technique to thwart cyber attacks by continuously changing the attack surface. 
+      It largely categorise into three: shuffle, diversity, and redundancy, and those techniques can be implemented in any layers of the system (e.g., think of OSI layers). 
+      To simulate this, we need to model three key components: the system, the attacker, and the MTD techniques. 
+      This project aims to implement these components by writing new modules and refactoring existing modules for improved user experience.</p>
     </div>
 </template>
 
