b32145621dd273486f82c3eee9b4fd111ac2a8dd
DanCUWA <chengdct@gmail.com>
2023-10-07 14:22:42 +0800

    Deleted removed routes

--- backend/mtdgui/routers/log.py
+++
@@ -1,7 +0,0 @@
-# from fastapi import APIRouter
-# # router = APIRouter(prefix="simulation",tags=["items"],responses={404: {"description":"Not found"}})
-# router = APIRouter(tags=["items"],responses={404: {"description":"Not found"}})
-
-# @router.get('/index')
-# async def read_home(): 
-#     return {"title":"Introduction"}
