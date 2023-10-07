35593d924bbbffd07c2b139944f01c258a5b66aa
DanCUWA <chengdct@gmail.com>
2023-10-07 14:20:50 +0800

    Commented out unnecessary routes

--- backend/mtdgui/routers/sim.py
+++ backend/mtdgui/routers/sim.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
-# router = APIRouter(prefix="simulation",tags=["items"],responses={404: {"description":"Not found"}})
-router = APIRouter(tags=["items"],responses={404: {"description":"Not found"}})
+# from fastapi import APIRouter
+# # router = APIRouter(prefix="simulation",tags=["items"],responses={404: {"description":"Not found"}})
+# router = APIRouter(tags=["items"],responses={404: {"description":"Not found"}})
 
-@router.get('/index')
-async def read_home(): 
-    return {"title":"Introduction"}
+# @router.get('/index')
+# async def read_home(): 
+#     return {"title":"Introduction"}
